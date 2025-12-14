3c3aca7986c8dbeb833d082166eb8be76f088192
Iustin Vasiliu <iustinv2.0@gmail.com>
2025-12-14 16:32:48 +0200

    chore: fix nav and error toast styling, prevent indexing, fix video page redirect

---
+++ extensions/video/src/pages/frontStore/all/RobotsMeta.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function RobotsMeta() {
+  return (
+    <meta name="robots" content="noindex, nofollow, noarchive, nosnippet" />
+  );
+}
+export const layout = {
+  areaId: "head",
+  sortOrder: 0,
+};
